7ebfb6e2d0e1367df3b9290098df46b9d2e6d57b
ppingale <prasad.pingale@accionlabs.com>
2019-10-12 22:18:12 +0530

    Initial Commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,27 @@
+Promise.all([
+	faceapi.nets.tinyFaceDetector.loadFromUri('http://127.0.0.1/blogposts/face-recognazation/models'),
+	faceapi.nets.faceLandmark68Net.loadFromUri('http://127.0.0.1/blogposts/face-recognazation/models'), // for eyes, mouth, nose etc.
+	faceapi.nets.faceRecognitionNet.loadFromUri('http://127.0.0.1/blogposts/face-recognazation/models'),
+	faceapi.nets.faceExpressionNet.loadFromUri('http://127.0.0.1/blogposts/face-recognazation/models'),
+]).then(startVideo);
+function startVideo() {
+	const video = document.getElementById('video');
+	navigator.getUserMedia({ video: {} }, stream => (video.srcObject = stream), err => console.error(err));
+}
+video.addEventListener('play', () => {
+	const canvas = faceapi.createCanvasFromMedia(video);
+	document.body.append(canvas);
+	const displaySize = { width: video.width, height: video.height };
+	faceapi.matchDimensions(canvas, displaySize);
+	setInterval(async () => {
+		const detections = await faceapi
+			.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+			.withFaceLandmarks()
+			.withFaceExpressions();
+		const resizedDetections = faceapi.resizeResults(detections, displaySize);
+		canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+		faceapi.draw.drawDetections(canvas, resizedDetections);
+		faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+		faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+	}, 200);
+});
